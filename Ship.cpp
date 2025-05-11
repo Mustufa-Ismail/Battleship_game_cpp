6be6d034e222430c3ef9515a944ba0c0e29d4fb8
Mustufa-Ismail <mustufaismail7942@gmail.com>
2025-05-11 22:56:36 +0500

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -1,32 +1,174 @@
 #include "Ship.h"
 
-Ship::Ship() : size(0), hitsTaken(0), coordCount(0) {}
+    void Ship::init(int len, sf::Texture shipTextures[5], float startX, float startY)
+    {
+        length = len;
+        gridX = -1;
+        gridY = -1;
+        isVertical = false;
+        isPlaced = false;
 
-Ship::Ship(int s) : size(s), hitsTaken(0), coordCount(0) {}
-
-void Ship::addCoordinate(int row, int col) {
-    if (coordCount < 5) {
-        coordinates[coordCount][0] = row;
-        coordinates[coordCount][1] = col;
-        coordCount++;
+        for (int i = 0; i < length; ++i)
+        {
+            if (i == 0)
+                parts[i].setTexture(shipTextures[0]);
+            else if (i == length - 1)
+                parts[i].setTexture(shipTextures[4]);
+            else
+                parts[i].setTexture(shipTextures[(i - 1) % 3 + 1]);
+            parts[i].setPosition(startX + i * partSize, startY);
+        }
     }
-}
 
-bool Ship::occupiesCell(int row, int col) const {
-    for (int i = 0; i < coordCount; i++) {
-        if (coordinates[i][0] == row && coordinates[i][1] == col)
-            return true;
+    void Ship::draw(sf::RenderWindow &window)
+    {
+        for (int i = 0; i < length; ++i)
+            window.draw(parts[i]);
     }
-    return false;
-}
 
-void Ship::registerHit() {
-    if (hitsTaken < size)
-        hitsTaken++;
-}
+    void Ship::rotate(sf::Texture shipTextures[5])
+    {
+        // Store the heads position to keep it fixed during rotation
+        sf::Vector2f headPos = parts[0].getPosition();
 
-bool Ship::isSunk() const {
-    return hitsTaken >= size;
+        for (int i = 0; i < length; ++i)
+        {
+            // Assign textures
+            if (i == 0)
+                parts[i].setTexture(shipTextures[0]);
+            else if (i == length - 1)
+                parts[i].setTexture(shipTextures[4]);
+            else
+                parts[i].setTexture(shipTextures[(i - 1) % 3 + 1]);
+
+            if (!isVertical)
+            {
+                // For horizontal to vertical
+                parts[i].setRotation(90);
+                parts[i].setOrigin(0, parts[i].getLocalBounds().height);
+                parts[i].setPosition(headPos.x, headPos.y + i * partSize);
+            }
+            else
+            {
+                // for vertical to horizontal
+                parts[i].setRotation(0);
+                parts[i].setOrigin(0, 0);
+                parts[i].setPosition(headPos.x + i * partSize, headPos.y);
+            }
+        }
+
+        isVertical = !isVertical; // Flip the state
+    }
+
+    bool Ship::canPlace(int gridX, int gridY, bool occupiedCells[gridSize][gridSize])
+    {
+        if (gridX < 0 || gridY < 0)
+            return false;
+
+        if (isVertical)
+        {
+            if (gridY + length > gridSize)
+                return false;
+            for (int i = 0; i < length; ++i)
+            {
+                if (occupiedCells[gridY + i][gridX])
+                    return false;
+            }
+        }
+        else
+        {
+            if (gridX + length > gridSize)
+                return false;
+            for (int i = 0; i < length; ++i)
+            {
+                if (occupiedCells[gridY][gridX + i])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    void Ship::markPosition(int gridX, int gridY, bool occupiedCells[gridSize][gridSize])
+    {
+        if (isVertical)
+        {
+            for (int i = 0; i < length; ++i)
+            {
+                occupiedCells[gridY + i][gridX] = true;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < length; ++i)
+            {
+                occupiedCells[gridY][gridX + i] = true;
+            }
+        }
+    }
+
+    void Ship::clearPosition(int gridX, int gridY, bool occupiedCells[gridSize][gridSize])
+    {
+        if (gridX < 0 || gridY < 0)
+            return;
+
+        if (isVertical)
+        {
+            for (int i = 0; i < length && (gridY + i) < gridSize; ++i)
+            {
+                occupiedCells[gridY + i][gridX] = false;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < length && (gridX + i) < gridSize; ++i)
+            {
+                occupiedCells[gridY][gridX + i] = false;
+            }
+        }
+    }
+
+    void Ship::setPosition(float x, float y)
+    {
+        for (int i = 0; i < length; ++i)
+        {
+            if (isVertical)
+            {
+                parts[i].setPosition(x, y + i * partSize);
+            }
+            else
+            {
+                parts[i].setPosition(x + i * partSize, y);
+            }
+        }
+    }
+
+    void Ship::setGridPosition(int x, int y)
+    {
+        gridX = x;
+        gridY = y;
+    }
+    sf::Vector2i Ship::getGridPosition() const
+    {
+        return sf::Vector2i(gridX, gridY);
+    }
+
+    // getter and setters
+    bool Ship::getIsPlaced() const { return isPlaced; }
+    void Ship::setIsPlaced(bool placed) { isPlaced = placed; }
+    bool Ship::getIsVertical() const { return isVertical; }
+    int Ship::getLength() const { return length; }
+    sf::FloatRect Ship::getBounds() const { return parts[0].getGlobalBounds(); }
+
+    sf::FloatRect Ship::getPartBounds(int index) const
+    {
+        if (index >= 0 && index < length)
+        {
+            return parts[index].getGlobalBounds();
+        }
+        return sf::FloatRect(); // Return an empty rectangle if index is invalid
+    }
+
+
 }
 
 int Ship::getSize() const {
