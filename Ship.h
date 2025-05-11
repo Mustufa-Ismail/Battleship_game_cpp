19f78ec4572b0a9563dfc40693d14833be7300aa
Mustufa-Ismail <mustufaismail7942@gmail.com>
2025-05-11 22:54:54 +0500

    Update Ship.h

--- Ship.h
+++ Ship.h
@@ -1,21 +1,39 @@
 #ifndef SHIP_H
 #define SHIP_H
 
-class Ship {
-private:
-    int size;
-    int hitsTaken;
-    int coordinates[5][2];
-    int coordCount;
+#include <SFML/Graphics.hpp>
+using namespace std;
+const int partSize = 30;
+const int maxShipLength = 5;
+const int gridSize = 10;
+const int cellSize = 30;
+const int numShips = 6;
 
-public:
-    Ship();
-    Ship(int s);
-    void addCoordinate(int row, int col);
-    bool occupiesCell(int row, int col) const;
-    void registerHit();
-    bool isSunk() const;
-    int getSize() const;
+class Ship {
+    private:
+    int length;
+    sf::Sprite parts[maxShipLength];
+    int gridX, gridY;
+    bool isVertical;
+    bool isPlaced;
+
+    public:
+    void init(int len, sf::Texture shipTextures[5], float startX, float startY);
+    void draw(sf::RenderWindow &window);
+    void rotate(sf::Texture shipTextures[5]);
+    bool canPlace(int gridX, int gridY, bool occupiedCells[gridSize][gridSize]);
+    void markPosition(int gridX, int gridY, bool occupiedCells[gridSize][gridSize]);
+    void clearPosition(int gridX, int gridY, bool occupiedCells[gridSize][gridSize]);
+    void setPosition(float x, float y);
+    void setGridPosition(int x, int y);
+    sf::Vector2i getGridPosition() const;
+    bool getIsPlaced() const;
+    void setIsPlaced(bool placed);
+    bool getIsVertical() const;
+    int getLength() const;
+    sf::FloatRect getBounds() const;
+    sf::FloatRect getPartBounds(int index) const;
+
 };
 
 #endif
