db5898b545d78cb0f3fafc5ed0eeb1a910d3f589
Mustufa-Ismail <mustufaismail7942@gmail.com>
2025-05-11 22:57:37 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <algorithm>
+#include "Ship.h"
+#include "Player.h"
+#include "Game.h"
+
+int main()
+{
+    Game game;
+    game.run();
+    return 0;
+}
